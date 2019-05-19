97b29257a6639fd26634cafffdcc7e4a7fb9a356
Andrew Mitchell <mitche50@gmail.com>
2019-05-19 16:03:00 -0400

    Update util.service.ts

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -206,7 +206,7 @@ function generateAccountKeyPair(accountSecretKeyBytes) {
   return nacl.sign.keyPair.fromSecretKey(accountSecretKeyBytes);
 }
 
-function getPublicAccountID(accountPublicKeyBytes, prefix = 'xrb') {
+function getPublicAccountID(accountPublicKeyBytes, prefix = 'nano') {
   const accountHex = util.uint8.toHex(accountPublicKeyBytes);
   const keyBytes = util.uint4.toUint8(util.hex.toUint4(accountHex)); // For some reason here we go from u, to hex, to 4, to 8??
   const checksum = util.uint5.toString(util.uint4.toUint5(util.uint8.toUint4(blake.blake2b(keyBytes, null, 5).reverse())));
@@ -241,7 +241,7 @@ function getAccountPublicKey(account) {
   return uint4ToHex(key_uint4);
 }
 
-function setPrefix(account, prefix = 'xrb') {
+function setPrefix(account, prefix = 'nano') {
   if (prefix === 'nano') {
     return account.replace('xrb_', 'nano_');
   } else {
